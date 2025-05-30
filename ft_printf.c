afa93cf461922600e151b15cc42cb4c98353b118
Lucas William Payet <lupayet@f6r7s1.clusters.42paris.fr>
2025-05-30 18:08:16 +0200

    allons y

--- ft_printf.c
+++ ft_printf.c
@@ -6,12 +6,47 @@
 /*   By: lupayet <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/21 10:35:20 by lupayet           #+#    #+#             */
-/*   Updated: 2025/05/21 10:43:37 by lupayet          ###   ########.fr       */
+/*   Updated: 2025/05/30 18:07:42 by lupayet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 
-int	ft_printf(const char *, ...)
+int	is_specifier(char c, char *s)
 {
+	while (*s)
+	{
+		if (*s == c)
+			return (1);
+		s++;
+	}
+	return (0);
+}
+
+int	ft_printf(const char *arg, ...)
+{
+	int	count;
+	va_list args;
+
+	count = 0;
+	va_start(args, arg);
+
+	while (*arg)
+	{
+		if (*arg != '%')
+		{
+			ft_putchar(*arg);
+			count++;
+		}
+		else
+		{
+			arg++;
+			if (is_specifier(arg, "cspdiuxX%"))
+			{
+				ft_putchar('%');
+				count++;
+			}
+
+		}
+	}
 }
